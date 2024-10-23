62151f9f1523ceb899f3b93f58b75f2d509f8e00
BHAGYA-PV <146536866+BHAGYA-PV@users.noreply.github.com>
2024-10-23 11:20:26 +0530

    first commit

---
+++ styles/js/index.js
@@ -0,0 +1,42 @@
+const countDownDate = new Date().getTime() + (5 * 24 * 60 * 60 * 1000);
+
+// Update the countdown every 1 second
+const x = setInterval(function() {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result
+  document.getElementById("days").innerHTML = days.toString().padStart(2,  '0');
+  document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+  document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+  document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+
+  // If the countdown is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
+
+
+// Grab elements
+const toggler = document.getElementById('navbar-toggler');
+const navbarContent = document.getElementById('navbarContent');
+
+// Add event listener for the toggler button
+toggler.addEventListener('click', function() {
+    // Toggle the 'show' class on the navbar content
+    navbarContent.classList.toggle('show');
+});
